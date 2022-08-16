1fbd3a52d335beda73a94efc26478e349448a37e
root <root@localhost.localdomain>
2022-08-16 02:35:27 -0400

    Done 11.2

---
+++ solutions/11_modules/task_11_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from task_11_1 import parse_cdp_neighbors
+
+def create_network_map(filenames):
+    topo={}
+    for f in filenames:
+        with open(f) as cdpn:
+            topo.update(parse_cdp_neighbors(cdpn.read()))
+    return topo
+
+infiles = [
+    "sh_cdp_n_sw1.txt",
+    "sh_cdp_n_r1.txt",
+    "sh_cdp_n_r2.txt",
+    "sh_cdp_n_r3.txt",
+]
+
+
+print(create_network_map(infiles))
